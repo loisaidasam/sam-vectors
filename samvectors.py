0055652e484850a1df258cb5c0897748f4b1c9d5
Sam Sandberg <sam.sandberg@gmail.com>
2017-03-24 14:23:21 -0400

    Vector normalization, tokenization, main() output, sample corpus

--- samvectors.py
+++ samvectors.py
@@ -1,21 +1,28 @@
+"""Simple AutoMated vectorization of "sentences" of "words" to create custom
+embeddings based on n-gram co-occurence.
+"""
 
 import sys
 
 import numpy as np
+from scipy.linalg import norm
 
 
 NGRAM_SIZE = 3
 
 
-def get_ngrams(sentence):
-    words = sentence.split()
+def tokenize(sentence):
     # TODO: Use stemmer
+    return sentence.strip().split()
+
+
+def get_ngrams(sentence):
     ngrams = []
-    for i in xrange(len(words)):
-        ngram = words[i:i + NGRAM_SIZE]
+    for i in xrange(len(sentence)):
+        ngram = sentence[i:i + NGRAM_SIZE]
         if len(ngram) < NGRAM_SIZE:
             break
-        ngrams.append(ngram)
+        ngrams.append(tuple(ngram))
     return ngrams
 
 
@@ -30,8 +37,7 @@ def get_all_ngrams(sentences):
 
 
 def normalize_vector(vector):
-    # TODO: Implement
-    return vector
+    return vector / norm(vector)
 
 
 def vectorize(sentences):
@@ -50,8 +56,17 @@ def main():
     corpus_filename = sys.argv[1]
     vector_filename = sys.argv[2]
     with open(corpus_filename, 'r') as fp:
-        sentences = [row for row in fp]
+        sentences = []
+        for sentence in fp:
+            words = tokenize(sentence)
+            sentences.append(words)
     vectors = vectorize(sentences)
+    for sentence, vector in zip(sentences, vectors):
+        # print sentence, "\n", vector, "\n"
+        print sentence, "\n"
+        for sentence2, vector2 in zip(sentences, vectors):
+            score = vector.T.dot(vector2)
+            print "\t", score, sentence2
     np.savez(vector_filename, vectors=vectors, sentences=sentences)
 
 
